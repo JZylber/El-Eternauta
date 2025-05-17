19575ded44490b14132bc51d20031c0bc9f155c0
JZylber <zylberjulian@gmail.com>
2025-05-17 11:37:30 -0300

    Initial Commit

---
+++ main.js
@@ -0,0 +1,10 @@
+let personas = ["Juan", "Favalli", "Elena", "Inga", "Pablo", "Ana"];
+let lugares = [
+  "Supermercado Chino",
+  "Ferretería",
+  "Escuela",
+  "Almacén",
+  "Farmacia",
+  "Bomberos",
+  "Policía",
+];
